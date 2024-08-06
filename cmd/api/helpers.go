41cb71e84bc7eba6fc4c96da515ebb0ae98ab4fc
brahim <brahim.boussaa1998@gmail.com>
2024-08-06 15:07:18 +0100

    add helpers

---
+++ cmd/api/helpers.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type envelope map[string]interface{}
+
+func (app *application) ReadJSON(w http.ResponseWriter, r *http.Request, dest interface{}) error {
+	const max = 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(max))
+
+	dc := json.NewDecoder(r.Body)
+	dc.DisallowUnknownFields()
+
+	err := dc.Decode(dest)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		// returning helpful message based on the error
+		switch {
+		// syntax error
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+		// badly formatted
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+		// empty body
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		// unknown field
+		case strings.HasPrefix(err.Error(), "json: unkown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unkdown field")
+			return fmt.Errorf("body contains unknow key %s", fieldName)
+		// check if the body too large
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", max)
+		default:
+			return err
+		}
+	}
+	// check if there's multiple JSON values
+	err = dc.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+	return nil
+}
+
+// Handle the json response
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	jsn, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	jsn = append(jsn, '\n')
+	for k, v := range headers {
+		w.Header()[k] = v
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(jsn)
+	return nil
+}
