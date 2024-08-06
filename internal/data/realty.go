f1a13481365573f5b2ab904aa26d85621ef502a9
brahim <brahim.boussaa1998@gmail.com>
2024-08-06 16:06:02 +0100

    add endpoints to add realty and to get all realties

--- internal/data/realty.go
+++ internal/data/realty.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+	"context"
 	"database/sql"
+	"time"
 )
 
 type Realty struct {
@@ -26,5 +28,50 @@ func (m RealtyModel) Insert(realty *Realty) error {
 	args := []interface{}{
 		realty.ListingType, realty.PromoType, realty.URL,
 		realty.ProjectName, realty.DisplayAddress}
-	return m.DB.QueryRow(query, args...).Scan(&realty.ID)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&realty.ID)
 }
+func (m RealtyModel) GetAll() ([]*Realty, error) {
+	query := "SELECT * FROM realty LIMIT 200"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	realties := []*Realty{}
+	for rows.Next() {
+		var realty Realty
+		err := rows.Scan(&realty.ID, &realty.ListingType, &realty.PromoType,
+			&realty.URL, &realty.ProjectName, &realty.DisplayAddress)
+		if err != nil {
+			return nil, err
+		}
+		realties = append(realties, &realty)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return realties, nil
+}
+
+// Validate Realty inpute
+
+// func ValidateRealty(v *validator.Validator, realty *Realty) {
+// 	v.Check(realty.Title != "", "title", "Must be provided")
+// 	v.Check(len(realty.Title) <= 500, "title", "Must not be more than 500 bytes long")
+//
+// 	v.Check(movie.Year != 0, "year", "Must be provided")
+// 	v.Check(movie.Year >= 1888, "year", "Must be greater than 1888")
+// 	v.Check(movie.Year <= int32(time.Now().Year()), "year", "Must not be in the future")
+//
+// 	v.Check(movie.Runtime != 0, "runtime", "Must be provided")
+// 	v.Check(movie.Runtime > 0, "runtime", "Must be a positive integer")
+//
+// 	v.Check(movie.Genres != nil, "genres", "Must be provided")
+// 	v.Check(len(movie.Genres) >= 1, "genres", "Must contain at least 1")
+// 	v.Check(len(movie.Genres) <= 5, "genres", "Must not contain more than 5")
+// 	v.Check(validator.Unique(movie.Genres), "genres", "Must not contain duplicate values")
+// }
