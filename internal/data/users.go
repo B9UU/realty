b352d953099410f864616ff62317be90f4960bde
brahim <brahim.boussaa1998@gmail.com>
2024-08-15 01:09:03 +0100

    feat: add users table, users Insert method, password hashing and input validation

---
+++ internal/data/users.go
@@ -0,0 +1,115 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/b9uu/realty/internal/validator"
+	"github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrDuplicateEmail = errors.New("duplicate email")
+	AnonymousUser     = &User{}
+)
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+type UserInterface interface {
+	Insert(user *User) error
+}
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+type password struct {
+	plainText *string
+	hash      []byte
+}
+
+// sets plainPassword and it's hash to p
+func (p *password) Set(plainPassword string) error {
+	pswrd, err := bcrypt.GenerateFromPassword([]byte(plainPassword), 12)
+	if err != nil {
+		return err
+	}
+	p.plainText = &plainPassword
+	p.hash = pswrd
+	return nil
+}
+
+// checks if plainText hash == p.hash
+func (p *password) Match(plainPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plainPassword))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+// validate the email
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+
+}
+
+// validate the password
+func ValidPassword(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) < 72, "password", "must not be more than 72 bytes long")
+}
+
+// validate user
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+	ValidateEmail(v, user.Email)
+	if user.Password.plainText != nil {
+		ValidPassword(v, *user.Password.plainText)
+	}
+
+	// password.hash shouldn't be nil so we panic rather than returning an error
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
+}
+
+func (u UserModel) Insert(user *User) error {
+	query := `
+		INSERT INTO users (name, email, password_hash, activated)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version
+	`
+	args := []interface{}{
+		user.Name, user.Email, user.Password.hash, user.Activated,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	err := u.DB.QueryRowContext(ctx, query, args...).Scan(
+		&user.ID, &user.CreatedAt, &user.Version,
+	)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == pq.ErrorCode("23505") {
+			return ErrDuplicateId
+		}
+		return err
+	}
+	return nil
+}
